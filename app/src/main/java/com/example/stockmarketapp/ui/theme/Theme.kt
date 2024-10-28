4382ca675e21af7e0bbd8cd6a3105b205941ef05
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-10-28 11:28:10 -0600

    Init branch

--- app/src/main/java/com/example/stockmarketapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/stockmarketapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.stockmarketapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
