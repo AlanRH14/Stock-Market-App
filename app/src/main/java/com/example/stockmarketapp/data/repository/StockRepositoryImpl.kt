fc4fd94b06de8a3d79220d7a5609929a07919fb1
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-10-29 12:05:27 -0600

    Create logic for layer data, request service and cache logic

---
+++ app/src/main/java/com/example/stockmarketapp/data/repository/StockRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.example.stockmarketapp.data.repository
+
+import com.example.stockmarketapp.data.local.StockDatabase
+import com.example.stockmarketapp.data.mapper.toCompanyListing
+import com.example.stockmarketapp.data.remote.api.StockApi
+import com.example.stockmarketapp.domain.model.CompanyListing
+import com.example.stockmarketapp.domain.repository.StockRepository
+import com.example.stockmarketapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class StockRepositoryImpl @Inject constructor(
+    val api: StockApi,
+    val db: StockDatabase,
+) : StockRepository {
+
+    private val dao = db.dao
+
+    override suspend fun getCompanyListing(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<List<CompanyListing>>> {
+        return flow {
+            emit(Resource.Loading(isLoading = true))
+            val localListings = dao.searchCompanyListing(query = query)
+            emit(
+                Resource.Success(
+                    data = localListings.map { it.toCompanyListing() }
+                )
+            )
+
+            val isDbEmpty = localListings.isEmpty() && query.isBlank()
+            val shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote
+            if (shouldJustLoadFromCache) {
+                emit(Resource.Loading(isLoading = false))
+                return@flow
+            }
+
+            try {
+                val response = api.getStockListings()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                emit(Resource.Error(data = null, message = "Couldn´t load data"))
+            } catch (e: HttpException) {
+                emit(Resource.Error(data = null, message = "Couldn´t load data"))
+            }
+        }
+    }
+}
