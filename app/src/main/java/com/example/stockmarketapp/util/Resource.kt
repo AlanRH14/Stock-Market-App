e64cef73617b5ac0525f4f1f3b74d8cc96272b0f
Alan Andrés Rosas Hernández <84395371+AlanRH14@users.noreply.github.com>
2024-12-12 16:54:08 -0600

    Revert "Company listings screen"

--- app/src/main/java/com/example/stockmarketapp/util/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.stockmarketapp.util
-
-sealed class Resource<T>(val data: T? = null, message: String? = null) {
-    class Success<T>(data: T?) : Resource<T>(data = data)
-    class Error<T>(data: T?, message: String) : Resource<T>(data = data, message = message)
-    class Loading<T>(val isLoading: Boolean = true) : Resource<T>(data = null, message = null)
-}
