ccaa3a1d0121668e5875348f64d41f8f08701690
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-12-11 18:09:13 -0600

    Init ui CompanyItem

---
+++ app/src/main/java/com/example/stockmarketapp/ui/theme/Style.kt
@@ -0,0 +1,14 @@
+package com.example.stockmarketapp.ui.theme
+
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+object Style {
+    val NAME_ITEM = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold
+    )
+
+}
