88e12aa46a9819320ae347317359b7bbadfd542c
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-12-16 10:46:05 -0600

    Settings models dto y domain model

---
+++ app/src/main/java/com/example/stockmarketapp/domain/CompanyInfo.kt
@@ -0,0 +1,9 @@
+package com.example.stockmarketapp.domain
+
+data class CompanyInfo(
+    val symbol: String,
+    val description: String,
+    val name: String,
+    val country: String,
+    val industry: String
+)
