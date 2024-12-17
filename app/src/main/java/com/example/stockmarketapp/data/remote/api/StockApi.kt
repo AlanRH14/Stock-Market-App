29b73cc5b3f495dacd6a9189ee8d7a9332e29d1c
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-12-17 15:57:04 -0600

    Settings getCompanyInfo in StockAPI

--- app/src/main/java/com/example/stockmarketapp/data/remote/api/StockApi.kt
+++ app/src/main/java/com/example/stockmarketapp/data/remote/api/StockApi.kt
@@ -1,6 +1,7 @@
 package com.example.stockmarketapp.data.remote.api
 
 import com.example.stockmarketapp.data.remote.api.ApiConstants.API_KEY
+import com.example.stockmarketapp.data.remote.dto.CompanyInfoDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,7 +10,7 @@ interface StockApi {
 
     @GET("query?function=LISTING_STATUS")
     suspend fun getStockListings(
-        @Query("apikey") apikey: String = API_KEY
+        @Query("apikey") apikey: String = API_KEY,
     ): ResponseBody
 
     @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
@@ -17,4 +18,10 @@ interface StockApi {
         @Query("symbol") symbol: String,
         @Query("apikey") apikey: String = API_KEY,
     )
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apikey: String = API_KEY,
+    ): CompanyInfoDto
 }
