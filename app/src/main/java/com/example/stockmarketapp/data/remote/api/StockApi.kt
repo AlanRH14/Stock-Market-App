789ec162dd585125584e70e96e7faa80dcc9d5a2
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-12-17 12:51:06 -0600

    Settings getIntradayInfo in StockAPI

--- app/src/main/java/com/example/stockmarketapp/data/remote/api/StockApi.kt
+++ app/src/main/java/com/example/stockmarketapp/data/remote/api/StockApi.kt
@@ -9,6 +9,12 @@ interface StockApi {
 
     @GET("query?function=LISTING_STATUS")
     suspend fun getStockListings(
-        @Query("apikey") apikey : String = API_KEY
+        @Query("apikey") apikey: String = API_KEY
     ): ResponseBody
+
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apikey: String = API_KEY,
+    )
 }
