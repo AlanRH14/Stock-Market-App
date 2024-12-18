1d8146429bb8314452db3fc2fde0253a3c3190d3
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-12-18 16:59:40 -0600

    Settings CompanyInfoViewModel

--- app/src/main/java/com/example/stockmarketapp/presentation/company_info/CompanyInfoViewModel.kt
+++ app/src/main/java/com/example/stockmarketapp/presentation/company_info/CompanyInfoViewModel.kt
@@ -1,9 +1,16 @@
 package com.example.stockmarketapp.presentation.company_info
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.stockmarketapp.domain.repository.StockRepository
+import com.example.stockmarketapp.domain.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,4 +19,54 @@ class CompanyInfoViewModel @Inject constructor(
     private val repository: StockRepository
 ) : ViewModel() {
 
+    var state by mutableStateOf(CompanyInfoState())
+
+    init {
+        viewModelScope.launch {
+            val symbol = savedStateHandle.get<String>("symbol") ?: return@launch
+            state = state.copy(isLoading = true)
+            val companyResult = async { repository.getCompanyInfo(symbol = symbol) }
+            val intradayInfoResult = async { repository.getIntradayInfo(symbol = symbol) }
+
+            when (val result = companyResult.await()) {
+                is Resource.Success -> {
+                    state = state.copy(
+                        company = result.data,
+                        isLoading = false,
+                        error = null,
+                    )
+                }
+
+                is Resource.Error -> {
+                    state = state.copy(
+                        error = result.message,
+                        isLoading = false,
+                        company = null,
+                    )
+                }
+
+                else -> Unit
+            }
+
+            when (val result = intradayInfoResult.await()) {
+                is Resource.Success -> {
+                    state = state.copy(
+                        stockInfo = result.data ?: emptyList(),
+                        isLoading = false,
+                        error = null,
+                    )
+                }
+
+                is Resource.Error -> {
+                    state = state.copy(
+                        error = result.message,
+                        isLoading = false,
+                        stockInfo = emptyList(),
+                    )
+                }
+
+                else -> Unit
+            }
+        }
+    }
 }
