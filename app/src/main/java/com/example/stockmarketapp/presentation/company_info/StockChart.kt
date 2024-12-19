dfe0b3ce21df993cabed17e6f0f70edadd2bad60
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-12-19 13:30:48 -0600

    fun StockChart

--- app/src/main/java/com/example/stockmarketapp/presentation/company_info/StockChart.kt
+++ app/src/main/java/com/example/stockmarketapp/presentation/company_info/StockChart.kt
@@ -1,9 +1,14 @@
 package com.example.stockmarketapp.presentation.company_info
 
+import android.graphics.Paint
+import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.sp
 import com.example.stockmarketapp.domain.model.IntradayInfo
 import kotlin.math.roundToInt
 
@@ -22,5 +27,17 @@ fun StockChart(
     }
     val lowValue = remember(info) {
         (info.minOfOrNull { it.close }?.toInt() ?: 0)
+    }
+    val density = LocalDensity.current
+    val textPaint = remember {
+        Paint().apply {
+            color = android.graphics.Color.WHITE
+            textAlign = Paint.Align.CENTER
+            textSize = density.run { 12.sp.toPx() }
+        }
+    }
+
+    Canvas(modifier = modifier) {
+        val spacePerHour = (size.width - spacing) / info.size
     }
 }
