1741b5a68f7f1783394fb78f39b9f89ccdae73fa
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-12-19 12:38:10 -0600

    fun StockChart

--- app/src/main/java/com/example/stockmarketapp/presentation/company_info/StockChart.kt
+++ app/src/main/java/com/example/stockmarketapp/presentation/company_info/StockChart.kt
@@ -1,8 +1,26 @@
 package com.example.stockmarketapp.presentation.company_info
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.stockmarketapp.domain.model.IntradayInfo
+import kotlin.math.roundToInt
 
 @Composable
-fun StockChart() {
-
+fun StockChart(
+    modifier: Modifier = Modifier,
+    info: List<IntradayInfo> = emptyList(),
+    graphColor: Color = Color.Green
+) {
+    val spacing = 100F
+    val transparentGraphColor = remember {
+        graphColor.copy(alpha = 0.5F)
+    }
+    val upperValue = remember(info) {
+        (info.maxOfOrNull { it.close }?.plus(1)?.roundToInt() ?: 0)
+    }
+    val lowValue = remember(info) {
+        (info.minOfOrNull { it.close }?.toInt() ?: 0)
+    }
 }
