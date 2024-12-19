27560f3901f215cfcf0d2e25e7c5440eeabeeb62
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-12-19 17:09:57 -0600

    fun StockChart

--- app/src/main/java/com/example/stockmarketapp/presentation/company_info/StockChart.kt
+++ app/src/main/java/com/example/stockmarketapp/presentation/company_info/StockChart.kt
@@ -1,17 +1,20 @@
 package com.example.stockmarketapp.presentation.company_info
 
 import android.graphics.Paint
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.sp
 import com.example.stockmarketapp.domain.model.IntradayInfo
+import java.util.Calendar
 import kotlin.math.roundToInt
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun StockChart(
     modifier: Modifier = Modifier,
@@ -39,5 +42,12 @@ fun StockChart(
 
     Canvas(modifier = modifier) {
         val spacePerHour = (size.width - spacing) / info.size
+        val calendar = Calendar.getInstance()
+        (0 until info.size - 1 step 2).forEach { i ->
+            val intradayInfo = info[i]
+            calendar.time = intradayInfo.date
+            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        }
     }
 }
