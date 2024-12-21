2a2f3927d35da1c9d7d3d28ca8d000ccebe4fa2b
Alan Andres Rosas Hernández <alanarh.14@gmail.com>
2024-12-21 00:16:29 -0600

    Final setting Stock chart

--- app/src/main/java/com/example/stockmarketapp/presentation/company_info/StockChart.kt
+++ app/src/main/java/com/example/stockmarketapp/presentation/company_info/StockChart.kt
@@ -6,11 +6,15 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.asAndroidPath
 import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.stockmarketapp.domain.model.IntradayInfo
 import java.lang.Math.round
@@ -100,5 +104,25 @@ fun StockChart(
                 lineTo(spacing, size.height - spacing)
                 close()
             }
+
+        drawPath(
+            path = fillPath,
+            brush = Brush.verticalGradient(
+                colors = listOf(
+                    transparentGraphColor,
+                    Color.Transparent
+                ),
+                endY = size.height - spacing
+            ),
+        )
+
+        drawPath(
+            path = strokePath,
+            color = graphColor,
+            style = Stroke(
+                width = 3.dp.toPx(),
+                cap = StrokeCap.Round
+            )
+        )
     }
 }
