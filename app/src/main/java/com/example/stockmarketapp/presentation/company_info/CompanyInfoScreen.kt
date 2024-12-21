f915ab377392887c6ebd070a22e829b7a7f9e778
Alan Andres Rosas Hernández <alanarh.14@gmail.com>
2024-12-21 02:16:36 -0600

    Setting CompanyInfoScreen

---
+++ app/src/main/java/com/example/stockmarketapp/presentation/company_info/CompanyInfoScreen.kt
@@ -0,0 +1,110 @@
+package com.example.stockmarketapp.presentation.company_info
+
+import android.widget.Space
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.stockmarketapp.ui.theme.DarkBlue
+
+@Composable
+fun CompanyInfoScreen(
+    modifier: Modifier = Modifier,
+    symbol: String,
+    viewModel: CompanyInfoViewModel = hiltViewModel(),
+) {
+
+    val state = viewModel.state
+
+    if (state.error != null) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(DarkBlue)
+                .padding(16.dp)
+        ) {
+            state.company?.let { company ->
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = company.name,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    overflow = TextOverflow.Ellipsis,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = company.symbol,
+                    fontStyle = FontStyle.Italic,
+                    fontSize = 14.sp,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                HorizontalDivider(thickness = 2.dp)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Industry: ${company.industry}",
+                    fontSize = 14.sp,
+                    overflow = TextOverflow.Ellipsis,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Country: ${company.country}",
+                    fontSize = 14.sp,
+                    overflow = TextOverflow.Ellipsis,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                HorizontalDivider(thickness = 2.dp)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = company.description,
+                    fontSize = 14.sp,
+                    overflow = TextOverflow.Ellipsis,
+                )
+                if (state.stockInfo.isNotEmpty()) {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(text = "Market Summary")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    StockChart(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(250.dp)
+                            .align(Alignment.CenterHorizontally),
+                        info = state.stockInfo,
+                    )
+                }
+            }
+        }
+    }
+
+    Box(modifier = modifier.fillMaxWidth()) {
+        if (state.isLoading) {
+            CircularProgressIndicator()
+        } else if (state.error != null) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colorScheme.error,
+            )
+        }
+    }
+}
