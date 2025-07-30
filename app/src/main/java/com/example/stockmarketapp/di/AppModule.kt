c557b2a9ef96601cee253228e593b0c2f681f9df
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-30 09:39:34 -0600

    Config kotlin serialization project part. 2.

--- app/src/main/java/com/example/stockmarketapp/di/AppModule.kt
+++ app/src/main/java/com/example/stockmarketapp/di/AppModule.kt
@@ -4,25 +4,22 @@ import android.app.Application
 import androidx.room.Room
 import com.example.stockmarketapp.data.local.StockDatabase
 import com.example.stockmarketapp.data.remote.api.ApiConstants.BASE_URL
-import com.example.stockmarketapp.data.remote.api.StockApi
 import com.example.stockmarketapp.domain.utils.Constants.DB_NAME
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    val json = Json {
+    private val json = Json {
         coerceInputValues = true
         ignoreUnknownKeys = true
     }
@@ -31,18 +28,12 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providesStockApi(): StockApi =
+    fun providesStockApi(): Retrofit =
         Retrofit
             .Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(
-                MoshiConverterFactory.create(
-                    Moshi.Builder()
-                        .add(KotlinJsonAdapterFactory()).build()
-                )
-            )
+            .addConverterFactory(json.asConverterFactory(contentType = contentType))
             .build()
-            .create()
 
     @Provides
     @Singleton
