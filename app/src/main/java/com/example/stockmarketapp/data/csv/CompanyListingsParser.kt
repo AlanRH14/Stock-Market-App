c47021408a28527b5ee34e232192555abb500cef
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-30 10:17:32 -0600

    Config kotlin serialization project part. 9.

--- app/src/main/java/com/example/stockmarketapp/data/csv/CompanyListingsParser.kt
+++ app/src/main/java/com/example/stockmarketapp/data/csv/CompanyListingsParser.kt
@@ -6,11 +6,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.InputStream
 import java.io.InputStreamReader
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class CompanyListingsParser @Inject constructor() : CSVParser<CompanyListing> {
+class CompanyListingsParser : CSVParser<CompanyListing> {
 
     override suspend fun parse(stream: InputStream): List<CompanyListing> {
         val csvReader = CSVReader(InputStreamReader(stream))
