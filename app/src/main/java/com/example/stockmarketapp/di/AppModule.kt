a0c6eb0aa9da11238e8317e499abb9c4d394b605
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-30 10:41:26 -0600

    Config kotlin serialization project part. 25.

--- app/src/main/java/com/example/stockmarketapp/di/AppModule.kt
+++ app/src/main/java/com/example/stockmarketapp/di/AppModule.kt
@@ -1,21 +1,9 @@
 package com.example.stockmarketapp.di
 
-import androidx.room.Room
-import com.example.stockmarketapp.data.local.StockDatabase
-import com.example.stockmarketapp.data.remote.api.ApiConstants.BASE_URL
-import com.example.stockmarketapp.data.remote.api.StockApi
-import com.example.stockmarketapp.domain.utils.Constants.DB_NAME
 import com.example.stockmarketapp.presentation.company_info.CompanyInfoViewModel
 import com.example.stockmarketapp.presentation.company_listing.CompanyListingsViewModel
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
-import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 val appModule = module {
     viewModel { CompanyListingsViewModel(get()) }
