ef3125cc52838201d1814158065ce36360de966d
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-30 13:32:53 -0600

    Refactor part. 7.

--- app/src/main/java/com/example/stockmarketapp/data/mapper_impl/IntradayInfoMapper.kt
+++ app/src/main/java/com/example/stockmarketapp/data/mapper_impl/IntradayInfoMapper.kt
@@ -1,19 +1,23 @@
 package com.example.stockmarketapp.data.mapper_impl
 
+import com.example.stockmarketapp.common.ApiMapper
 import com.example.stockmarketapp.data.remote.dto.IntradayInfoDto
 import com.example.stockmarketapp.domain.model.IntradayInfo
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-fun IntradayInfoDto.toIntradayInfo(): IntradayInfo {
-    val pattern = "yyyy-MM-dd HH:mm:ss"
-    val formatter = SimpleDateFormat(pattern, Locale.getDefault())
-    val dateParse = formatter.parse(timestamp)?.time ?: 0L
-    val dateFormat = Date(dateParse)
+class IntradayInfoDomainMapper : ApiMapper<IntradayInfoDto, IntradayInfo> {
 
-    return IntradayInfo(
-        date = dateFormat,
-        close = close
-    )
+    override fun mapToDomain(apiDto: IntradayInfoDto): IntradayInfo {
+        val pattern = "yyyy-MM-dd HH:mm:ss"
+        val formatter = SimpleDateFormat(pattern, Locale.getDefault())
+        val dateParse = formatter.parse(apiDto.timestamp)?.time ?: 0L
+        val dateFormat = Date(dateParse)
+
+        return IntradayInfo(
+            date = dateFormat,
+            close = apiDto.close
+        )
+    }
 }
