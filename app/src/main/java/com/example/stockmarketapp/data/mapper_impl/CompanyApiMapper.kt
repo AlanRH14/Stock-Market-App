a7a079bb70e6ebccb0da1eca18b429b6b8246ed9
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-30 13:19:45 -0600

    Refactor part. 4.

--- app/src/main/java/com/example/stockmarketapp/data/mapper_impl/CompanyApiMapper.kt
+++ app/src/main/java/com/example/stockmarketapp/data/mapper_impl/CompanyApiMapper.kt
@@ -1,16 +1,20 @@
 package com.example.stockmarketapp.data.mapper_impl
 
+import com.example.stockmarketapp.common.ApiMapper
 import com.example.stockmarketapp.data.local.CompanyListingEntity
 import com.example.stockmarketapp.data.remote.dto.CompanyInfoDto
 import com.example.stockmarketapp.domain.CompanyInfo
 import com.example.stockmarketapp.domain.model.CompanyListing
 
-fun CompanyListingEntity.toCompanyListing(): CompanyListing {
-    return CompanyListing(
-        name = name,
-        symbol = symbol,
-        exchange = exchange,
-    )
+class CompanyApiMapper : ApiMapper<CompanyListingEntity, CompanyListing> {
+
+    override fun mapToDomain(apiDto: CompanyListingEntity): CompanyListing {
+        return CompanyListing(
+            name = apiDto.name,
+            symbol = apiDto.symbol,
+            exchange = apiDto.exchange,
+        )
+    }
 }
 
 fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
