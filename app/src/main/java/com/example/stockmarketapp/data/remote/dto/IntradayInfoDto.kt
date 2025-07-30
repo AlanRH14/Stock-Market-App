09bde684135eee53729b5bae939992440683da64
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-30 09:12:05 -0600

    Config kotlin serialization project part. 1.

--- app/src/main/java/com/example/stockmarketapp/data/remote/dto/IntradayInfoDto.kt
+++ app/src/main/java/com/example/stockmarketapp/data/remote/dto/IntradayInfoDto.kt
@@ -1,6 +1,12 @@
 package com.example.stockmarketapp.data.remote.dto
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class IntradayInfoDto(
+    @SerialName("timestamp")
     val timestamp: String,
+    @SerialName("close")
     val close: Double
 )
