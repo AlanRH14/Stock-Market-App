bc899d1eb91a5645232581b7dacd16453e1359e7
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-30 09:57:59 -0600

    Config kotlin serialization project part. 4.

--- app/src/main/java/com/example/stockmarketapp/di/AppModule.kt
+++ app/src/main/java/com/example/stockmarketapp/di/AppModule.kt
@@ -1,12 +1,10 @@
 package com.example.stockmarketapp.di
 
-import android.app.Application
 import androidx.room.Room
 import com.example.stockmarketapp.data.local.StockDatabase
 import com.example.stockmarketapp.data.remote.api.ApiConstants.BASE_URL
 import com.example.stockmarketapp.data.remote.api.StockApi
 import com.example.stockmarketapp.domain.utils.Constants.DB_NAME
-import dagger.Provides
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
