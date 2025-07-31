e27aded292db08f8f70758d5184538f1c6fe13e6
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-31 09:49:36 -0600

    Refactor Navigation part. 1.

--- app/src/main/java/com/example/stockmarketapp/navigation/Screen.kt
+++ app/src/main/java/com/example/stockmarketapp/navigation/Screen.kt
@@ -1,9 +1,10 @@
 package com.example.stockmarketapp.navigation
 
-import com.example.stockmarketapp.domain.utils.ScreenKeys.COMPANY_INFO
-import com.example.stockmarketapp.domain.utils.ScreenKeys.COMPANY_LISTINGS
+import kotlinx.serialization.Serializable
 
-sealed class Screen(val route: String) {
-    data object CompanyListings: Screen(route = COMPANY_LISTINGS)
-    data object CompanyInfo: Screen(route = COMPANY_INFO)
+sealed interface Screen {
+    @Serializable
+    data object CompanyListings: Screen
+    @Serializable
+    data object CompanyInfo: Screen
 }
