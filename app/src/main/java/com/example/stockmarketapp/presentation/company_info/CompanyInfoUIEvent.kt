ce61750aee222510b65fa0dc5ce3de3929d16d71
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-31 09:56:29 -0600

    Refactor Navigation part. 7.

--- app/src/main/java/com/example/stockmarketapp/presentation/company_info/CompanyInfoUIEvent.kt
+++ app/src/main/java/com/example/stockmarketapp/presentation/company_info/CompanyInfoUIEvent.kt
@@ -1,6 +1,6 @@
 package com.example.stockmarketapp.presentation.company_info
 
 interface CompanyInfoUIEvent {
-    data object OnGetCompanyInfo : CompanyInfoUIEvent
-    data object OnGetIntradayInfo : CompanyInfoUIEvent
+    data class OnGetCompanyInfo(val symbol: String) : CompanyInfoUIEvent
+    data class OnGetIntradayInfo(val symbol: String) : CompanyInfoUIEvent
 }
