5fed5a63211eebf2c0a3f1232fee6bb2c2c6c43a
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-31 13:41:47 -0600

    Refactor StockRepositoryImpl part. 7.

--- app/src/main/java/com/example/stockmarketapp/presentation/company_listing/CompanyListingsScreen.kt
+++ app/src/main/java/com/example/stockmarketapp/presentation/company_listing/CompanyListingsScreen.kt
@@ -1,17 +1,14 @@
 package com.example.stockmarketapp.presentation.company_listing
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
@@ -19,7 +16,6 @@ import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
